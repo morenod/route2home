1e5c269cc95fb558b3bceac490e4b681329549a4
g0trunks <morenodav@gmail.com>
2017-06-21 13:09:16 +0200

    Add roads to summary only if they are different from the last element

--- README.md
+++ README.md
@@ -10,4 +10,3 @@ If pip is not available on your machine, source code can be found on:
 https://github.com/googlemaps/google-maps-services-python
 
 # TODO
-- Remove only consecutives roads (now, if you take a road, go to another and back to the first, second item is removed)
--- route2home.py
+++ route2home.py
@@ -15,7 +15,7 @@ __VERSION__ = "0.1"
 
 parser = argparse.ArgumentParser(description="Route and travel time calculation", epilog="""
 
-GoogleMaps API Key required, obtain it on https://developers.google.com/console
+GoogleMaps API Key is required, obtain it on https://developers.google.com/console
 Report bugs on https://github.com/morenod/route2home
 """, formatter_class=RawTextHelpFormatter)
 parser.add_argument('--origin', metavar='ORIGIN', nargs='?', type=str, help='Departure Point', required=True)
@@ -50,15 +50,18 @@ directions_result = gmaps.directions(args.origin, args.destination, mode="drivin
 
 for i in directions_result:
     summary = []
-    clean2 = []
     time_to_home = i['legs'][0]['duration_in_traffic']['value']
     distance_to_home = i['legs'][0]['distance']['value']
+    # Use steps with more than 1000 meters to calculate summary
     for j in i['legs'][0]['steps']:
         if j['distance']['value'] > 1000:
+            # Extract road name from the step instructions. All roads are named with a capital letter, a dash and a number, into a bold html tag
             road = re.search(r"<b>[A-Z]-(\d*)</b>", j['html_instructions'])
+            # Remove the bold html tag
             clean = re.sub(r"<.?b>", "", road.string[road.start():road.end()])
-            summary.append(clean)
-    for k in summary:
-        if k not in clean2:
-            clean2.append(k)
-    print "Time to home: %s, %s kms via %s" %(time.strftime('%H:%M',time.gmtime(time_to_home)),distance_to_home/1000,'[%s]' % ' -> '.join(map(str, clean2)))
+            # Only add road to summary if it is different from the last added road (first is always added)
+            if len(summary) == 0:
+                summary.append(str(clean))
+            elif clean != summary[len(summary)-1]:
+                summary.append(str(clean))
+    print "Time to destination: %s, %s kms via %s" %(time.strftime('%H:%M',time.gmtime(time_to_home)),distance_to_home/1000,'[ %s ]' % ' -> '.join(map(str, summary)))
